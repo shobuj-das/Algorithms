9a7bf0df0cb95c55165996439c469feb629afef6
Shobuj Das <dasshobuj@gmail.com>
2025-04-25 19:53:30 +0600

    palindrome number solved

---
+++ Palindrome_number.py
@@ -0,0 +1,18 @@
+
+def checkPalindrome(number):
+    actualNumber = number
+    result = 0
+
+    while number > 0:
+        reminder = number % 10
+        result = (result * 10) + reminder
+        number = int(number / 10)
+
+    if actualNumber == result:
+        return "Palindrome"
+    else:
+        return "Not Palindrome"
+
+number = int(input("enter number: "))
+print(checkPalindrome(number))
+
