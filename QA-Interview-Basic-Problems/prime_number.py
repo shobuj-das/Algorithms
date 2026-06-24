2a62959bb76ddb257fd061b5c86596fce1a2342e
Shobuj Das <dasshobuj@gmail.com>
2026-06-24 22:16:59 +0600

    prime number

---
+++ QA-Interview-Basic-Problems/prime_number.py
@@ -0,0 +1,16 @@
+import math
+def is_prime_numbers(number):
+    if number <= 1:
+        return False
+    for i in range(2, int(math.sqrt(number))+1):
+        if number % i == 0:
+            return False
+    
+    return True
+
+if __name__== "__main__":
+    rangeFrom = int(input("Enter range from:"))
+    rangeTo = int(input("Enter range to: "))
+    for i in range(rangeFrom, rangeTo):
+        if is_prime_numbers(i):
+            print(i)
