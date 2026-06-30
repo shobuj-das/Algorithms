1983093edd7d09521c242a332d6f0bde445e2bac
Shobuj Das <dasshobuj@gmail.com>
2026-06-30 23:46:50 +0600

    finding large number between 3

--- QA-Interview-Basic-Problems/finding_large_number.py
+++ QA-Interview-Basic-Problems/finding_large_number.py
@@ -28,18 +28,28 @@ def find_second_large_number(arr):
 
     return second_large_number
 
+def finding_large_number_between_three(a,b,c):
+    if a > b and a > c:
+        return a
+    elif b > a and b > c:
+        return b
+    else:
+        return c
+
 
 if __name__=="__main__":
     # print(find_large_number([10,5,7,8,9,61,10,8]))
-    # print(find_second_large_numebr([10,5,7,8,9,65,10,80]))
+    # print(find_second_large_number([10,5,7,8,9,65,10,80]))
 
-    # print(f"Large number: {(find_large_number([10,5,7,8,9,61,10,8]))}")
-    # print(f"Second large number: {find_second_large_number([10,5,7,8,9,65,10,80])}")
+    print(f"Large number: {(find_large_number([10,5,7,8,9,61,10,8]))}")
+    print(f"Second large number: {find_second_large_number([10,5,7,8,9,65,10,80])}")
 
-    number_list = []
-    x = int(input("Enter a number: "))
-    for i in range(0,x):
-        number_list.append(int(input()))
+    # number_list = []
+    # x = int(input("Enter a number: "))
+    # for i in range(0,x):
+    #     number_list.append(int(input()))
     
-    print(f"Large number: {find_large_number(number_list)}")
-    print(f"Second large number: {find_second_large_number(number_list)}")
+    # print(f"Large number: {find_large_number(number_list)}")
+    # print(f"Second large number: {find_second_large_number(number_list)}")
+
+    print(finding_large_number_between_three(4,6,5))
