50cecac153706d7e13164b5fa7f26423cefcae2a
Shobuj Das <dasshobuj@gmail.com>
2026-07-03 23:48:37 +0600

    fibonacii using recursion solved

--- QA-Interview-Basic-Problems/Fibonacci_series.py
+++ QA-Interview-Basic-Problems/Fibonacci_series.py
@@ -19,10 +19,18 @@ def finding_fibonacci_series_using_recursion(number):
         raise ValueError("Given number should be non-negative")
     if not isinstance(number, int):
         raise ValueError("number must be an interger")
-    if number == 0 or number == 1:
+    if number == 0:
         return 0
+    if number == 1:
+        return 1
     
-    print()
+    return finding_fibonacci_series_using_recursion(number-1) + finding_fibonacci_series_using_recursion(number-2)
 
 if __name__ == "__main__":
-    print(fibonacci(3))
+    print(fibonacci(10))
+
+    fib_series = []
+    n_terms = 10
+    for x in range(n_terms):
+        fib_series.append(finding_fibonacci_series_using_recursion(x))
+    print(fib_series)
