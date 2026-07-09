62e1be6cf9c16d6d3b1cc5020347a07d0202bb39
Shobuj Das <dasshobuj@gmail.com>
2026-07-10 01:09:22 +0600

    longest common prefix solved

--- QA-Interview-Basic-Problems/Anagram.py
+++ QA-Interview-Basic-Problems/Anagram.py
@@ -1,30 +1,34 @@
-# def checkAnagram(str1, str2):
-#
-#     list1 = list(str1)
-#     list2 = list(str2)
-#
-#     list1.sort()
-#     list2.sort()
-#
-#     # str1 = ''.join(list1)
-#     # str2 = ''.join(list2)
-#     # if str1 == str2:
-#     #     return True
-#     # else:
-#     #     return False
-#
-#     for x in range(len(list1)):
-#         if list1[x] != list2[x]:
-#             return False
-#     return True
-#
-# print(checkAnagram("anagram", "nagaram"))
+def checkAnagram(str1, str2):
 
+    list1 = list(str1)
+    list2 = list(str2)
 
+    if len(list1) != len(list2):
+        return False
+    
+    list1.sort()
+    list2.sort()
+
+    for x in range(len(list1)):
+        if list1[x] != list2[x]:
+            return False
+    return True
+
+print(checkAnagram("anagram", "nagaram"))
+
+    # str1 = ''.join(list1)
+    # str2 = ''.join(list2)
+    # if str1 == str2:
+    #     return True
+    # else:
+    #     return False
 def checkAnagram(str1, str2):
     list1 = list(str1)
     list2 = list(str2)
 
+    if len(list1) != len(list2):
+        return False
+    
     for x in list1:
         if x in list2:
             list2.remove(x)
--- QA-Interview-Basic-Problems/longest_prefix.py
+++ QA-Interview-Basic-Problems/longest_prefix.py
@@ -3,33 +3,40 @@
 # Given a list of strings, find the longest common prefix shared by all strings.
 
 def longest_common_prefix(words):
-    if len(words) == 0:
-        return None
+    if not words:
+        return ""
     
-    first_word = words[0]
-    flag = True
+    stop = False
     prefix = []
-    index = 0
-    for ch in first_word:
-        for word in words:
-            if word[index] != ch:
-                flag = False
-        if flag:
+    first_word = words[0]
+    for i in range(len(first_word)):
+        for j in range(1,len(words)):
+            word = words[j]
+            if i >= len(word):
+                stop = True
+                break
+            elif first_word[i] != word[i]:
+                stop = True
+                break
+          
+        if stop:
             break
-        prefix.append(ch)
+        prefix.append(first_word[i])
+        
     prefix = "".join(prefix)
+
     return prefix
 
 if __name__=="__main__":
     print(longest_common_prefix(["flower", "flow", "flight"]))      # fl
-    # print(longest_common_prefix(["dog", "racecar", "car"]))         # ""
-    # print(longest_common_prefix(["interview", "internet", "internal"])) # inter
-    # print(longest_common_prefix(["apple", "apple"]))                # apple
-    # print(longest_common_prefix([""]))                              # ""
-    # print(longest_common_prefix([]))                                # ""
-    # print(longest_common_prefix(["a"]))                             # a
-    # print(longest_common_prefix(["ab", "a"]))                       # a
-    # print(longest_common_prefix(["abc", "abcd", "abcde"]))          # abc
-    # print(longest_common_prefix(["prefix", "pre", "prevent"]))      # pre
-    # print(longest_common_prefix(["same", "same", "same"]))          # same
-    # print(longest_common_prefix(["abc", "xyz"]))                    # ""
+    print(longest_common_prefix(["dog", "racecar", "car"]))         # ""
+    print(longest_common_prefix(["interview", "internet", "internal"])) # inter
+    print(longest_common_prefix(["apple", "apple"]))                # apple
+    print(longest_common_prefix([""]))                              # ""
+    print(longest_common_prefix([]))                                # ""
+    print(longest_common_prefix(["a"]))                             # a
+    print(longest_common_prefix(["ab", "a"]))                       # a
+    print(longest_common_prefix(["abc", "abcd", "abcde"]))          # abc
+    print(longest_common_prefix(["prefix", "pre", "prevent"]))      # pre
+    print(longest_common_prefix(["same", "same", "same"]))          # same
+    print(longest_common_prefix(["abc", "xyz"]))                    # ""
