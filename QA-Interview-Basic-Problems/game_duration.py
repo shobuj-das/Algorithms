15b785d0c2ea30c0d5db22ddcc3c0fa293b53c26
Shobuj Das <dasshobuj@gmail.com>
2026-07-21 23:42:03 +0600

    game hour

---
+++ QA-Interview-Basic-Problems/game_duration.py
@@ -0,0 +1,12 @@
+# https://judge.beecrowd.com/en/problems/view/1047
+    
+
+def count_duration_of_game(start_hour, start_min, finish_hour, finish_min):
+
+    if finish_hour < start_hour:
+        finish_hour += 12
+
+
+if __name__=="__main__":
+    # take input
+    count_duration_of_game()
