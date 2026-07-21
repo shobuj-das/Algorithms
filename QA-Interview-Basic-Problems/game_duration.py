11c1c9f3f96fd9f15d3dbba7efcd66082c2f0f2c
Shobuj Das <dasshobuj@gmail.com>
2026-07-22 00:40:42 +0600

    hour calculation

--- QA-Interview-Basic-Problems/game_duration.py
+++ QA-Interview-Basic-Problems/game_duration.py
@@ -1,12 +1,29 @@
 # https://judge.beecrowd.com/en/problems/view/1047
     
 
+def print_result(hour, minutes):
+    print(f"O JOGO DUROU {hour} HORA(S) E {minutes} MINUTO(S)")
+
+
 def count_duration_of_game(start_hour, start_min, finish_hour, finish_min):
 
-    if finish_hour < start_hour:
-        finish_hour += 12
+    # if finish_hour < start_hour:
+    #     finish_hour += 24
+    
+    minutes = (60-(start_hour-finish_hour)) if start_hour>=finish_hour else (60-(finish_hour-start_hour))
+
+    if start_hour >= finish_hour:
+        hour = 24 - start_hour - finish_hour
+    else:
+        hour = finish_hour - start_hour
+    
+    if finish_min < start_min:
+        hour -= 1
+
+    print_result(hour,minutes)
+
 
 
 if __name__=="__main__":
     # take input
-    count_duration_of_game()
+    count_duration_of_game(7,7,7,7)
